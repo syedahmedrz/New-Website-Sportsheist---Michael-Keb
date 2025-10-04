b10e278de886347caee82adb5b95fdf8c654cea7
Syed Ahmed <syedahmed.aviation@gmail.com>
2025-10-04 13:05:07 +0500

    update hamba

--- Js/navbar.js
+++ Js/navbar.js
@@ -3,5 +3,6 @@ let hambarger_menu = document.querySelector(".hambarger-menu");
 
 hambarger_menu.addEventListener("click", ()=>{
         nav_links.classList.toggle("active");
+    hambarger_menu.classList.toggle("active");
 
 })
