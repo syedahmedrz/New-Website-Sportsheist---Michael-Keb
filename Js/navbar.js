b5424d5873fb2677ff5f5dc8ab4473fa776e107b
Syed Ahmed <syedahmed.aviation@gmail.com>
2025-10-04 12:58:53 +0500

    Enable navbar toggle

---
+++ Js/navbar.js
@@ -0,0 +1,7 @@
+let nav_links = document.querySelector(".nav-links");
+let hambarger_menu = document.querySelector(".hambarger-menu");
+
+hambarger_menu.addEventListener("click", ()=>{
+        nav_links.classList.toggle("active");
+
+})
